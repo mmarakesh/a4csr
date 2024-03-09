2135f4f45ee0683193a230a49a1ff9446c9797f6
mmarakesh <marinamakarova749@gmail.com>
2024-03-09 13:37:04 +0100

    added router and logo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+import logo from './image/logo.png';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  return <Router>
+    <div className="nav">
+    <div className="nav-left">
+      <img src={logo} alt="logo" width="400px"/>
     </div>
-  );
+    <nav className="nav-right">
+      <Link to = "/o nama" className="link">O nama</Link>
+      <Link to = "/partneri" className="link">Partneri</Link>
+      <Link to = "/vjesti" className="link">Vjesti</Link>
+      <Link to = "/projekti" className="link">Projekti</Link>
+      <Link to = "/kontakt" className="link kontakt">Kontakt</Link>
+    </nav>
+    </div>
+  </Router>
 }
-
 export default App;
