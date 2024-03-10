c27079628222c295579d02020353b8613c9c7323
mmarakesh <marinamakarova749@gmail.com>
2024-03-10 14:57:55 +0100

    created input and button

---
+++ src/Components/Onama/Formspree.js
@@ -0,0 +1,14 @@
+import './Formspree.css';
+
+function Formspree() {
+    return(
+    <div className='formspree'>
+    <form>
+        <input className='formspree-input' type='email' placeholder='email'/>
+    </form>
+        <button className='formspree-btn'>Postanite naš partneri</button>
+    </div>
+    )
+}
+
+export default Formspree;
