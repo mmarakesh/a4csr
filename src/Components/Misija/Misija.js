1e85484073ecbc00a1cb10044e782ddef669a7ca
mmarakesh <marinamakarova749@gmail.com>
2024-03-15 00:59:13 +0100

    added substring to Onama and Misija

--- src/Components/Misija/Misija.js
+++ src/Components/Misija/Misija.js
@@ -1,7 +1,13 @@
+import { useState } from 'react';
 import './Misija.css';
 import image from './imagePeople.jpg';
 
 function Misija() {
+    const[showMore, setShowMore] = useState(false);
+    let misijaText = `Naša misija je promovisanje principa društveno odgovornog i održivog poslovanja, kao i doprinos integraciji Crne Gore i zemalja Zapadnog Balkana. 
+    Kroz usklađivanje ekonomskog rasta, socijalne inkluzije i zaštite životne sredine, te promociju ljudskih, građanskih, ekonomskih i socijalnih prava, posebno u privatnom sektoru, nastojimo stvoriti okruženje koje podstiče aktivno učešće mladih i zainteresovanih strana.
+    Kroz saradnju sa relevantnim subjektima i razvoj civilnog društva na lokalnom i državnom nivou, želimo postići razmjenu iskustava, pružiti stručnu pomoć i pratiti stanje održivog razvoja u skladu sa UN globalnim ciljevima.
+    Sve ovo radimo s ciljem unapređenja društveno odgovornog poslovanja i održivog razvoja, ističući važnost relevantnih pregovaračkih poglavlja i aktivnosti za postizanje održivog zajednice.`
     return(
         <div>
 <h1 className="header">Naša <span className='green-header'> misija i ciljevi</span></h1>
@@ -10,10 +16,8 @@ function Misija() {
             <img className='image-people' src={image} alt='people'/>
             </div>
             <div className='misija-text-box'>
-            <p> Naša misija je promovisanje principa društveno odgovornog i održivog poslovanja, kao i doprinos integraciji Crne Gore i zemalja Zapadnog Balkana. </p>
-            <p>Kroz usklađivanje ekonomskog rasta, socijalne inkluzije i zaštite životne sredine, te promociju ljudskih, građanskih, ekonomskih i socijalnih prava, posebno u privatnom sektoru, nastojimo stvoriti okruženje koje podstiče aktivno učešće mladih i zainteresovanih strana.</p>
-            <p>Kroz saradnju sa relevantnim subjektima i razvoj civilnog društva na lokalnom i državnom nivou, želimo postići razmjenu iskustava, pružiti stručnu pomoć i pratiti stanje održivog razvoja u skladu sa UN globalnim ciljevima.</p>
-            <p>Sve ovo radimo s ciljem unapređenja društveno odgovornog poslovanja i održivog razvoja, ističući važnost relevantnih pregovaračkih poglavlja i aktivnosti za postizanje održivog zajednice.</p>
+            <p>{showMore ? misijaText : misijaText.substring(0, 419) + "..."}</p>
+            <button className='misija-btn' onClick={() => setShowMore(!showMore)}>{showMore ? "← Saznaj manje" : "Saznaj više 🡒"} </button>
             </div>
         </div>
         </div>
--- src/Components/Onama/Onama.js
+++ src/Components/Onama/Onama.js
@@ -1,12 +1,16 @@
+import { useState } from 'react'
 import ContactForm from './ContactForm'
 import image from './imageHands.jpg'
 import './Onama.css'
 function Onama() {
+    const[showMore, setShowMore] = useState(false);
+    let onamaText = "Organizacija koja ima za cilj afirmisanje principa društveno odgovornog poslovanja i održivog poslovanja, te doprinosa integracionim procesima Crne Gore i zemljama Zapadnog Balkana kroz promovisanje podsticajne poslovne klime, njene vrijednosti i značaja, kao i ukazivanje na probleme postizanja društveno odgovornog poslovanja i održivog razvoja u Crnoj Gori, zemljama Zapadnog Balkana i usklađivanja ekonomskog rasta, inkluzivnog i zelenog rasta."
     return(
 <div className='onama'>
     <div className='box-onama-text'>
         <h1 className='onama-header'>NVO <span className='green-header'>“Asocijacija za društveno odgovorno poslovanje”</span></h1>
-        <span className='onama-text'>Organizacija koja ima za cilj afirmisanje principa društveno odgovornog poslovanja i održivog poslovanja, te doprinosa integracionim procesima Crne Gore i zemljama Zapadnog Balkana kroz promovisanje podsticajne poslovne klime, njene vrijednosti i značaja, kao i ukazivanje na probleme postizanja društveno odgovornog poslovanja i održivog razvoja u Crnoj Gori, zemljama Zapadnog Balkana i usklađivanja ekonomskog rasta, inkluzivnog i zelenog rasta.</span>
+        <p className='onama-text'>{showMore ? onamaText : onamaText.substring(0,186) + "..."}</p>
+        <button className='onama-btn' onClick={() => setShowMore(!showMore)}>{showMore ? "← Saznaj manje" : "Saznaj više 🡒"} </button>
         <ContactForm/>
     </div>
     <div>
