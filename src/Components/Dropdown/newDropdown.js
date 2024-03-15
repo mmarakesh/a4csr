a539af4773919f395dda7d8206629927aadaf1d9
OxySokolova <poksanaa16@gmail.com>
2024-03-14 23:34:35 -0500

    onama stile

--- src/Components/Dropdown/newDropdown.js
+++ src/Components/Dropdown/newDropdown.js
@@ -1,19 +1,20 @@
 import Dropdown from 'react-bootstrap/Dropdown';
+import './Dropdown.css'
 
 function newDropdown() {
-  return (
+    return (
     <Dropdown>
-      <Dropdown.Toggle variant="none" className ='fs-4' id="dropdown-basic">
-        O nama
-      </Dropdown.Toggle>
+        <Dropdown.Toggle variant="none" className ='link' id="dropdown-basic">
+            O nama
+    </Dropdown.Toggle>
 
-      <Dropdown.Menu>
+    <Dropdown.Menu>
         <Dropdown.Item href="#/action-1">Tim</Dropdown.Item>
         <Dropdown.Item href="#/action-2">Poslovna documentacija</Dropdown.Item>
         <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-      </Dropdown.Menu>
+    </Dropdown.Menu>
     </Dropdown>
-  );
+);
 }
 
 export default newDropdown;
