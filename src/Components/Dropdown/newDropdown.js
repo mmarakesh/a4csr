575a6b26c829cdee8c44c06d0827121241fd7323
mmarakesh <marinamakarova749@gmail.com>
2024-03-15 08:45:18 +0100

    fixed bug

--- src/Components/Dropdown/newDropdown.js
+++ src/Components/Dropdown/newDropdown.js
@@ -11,7 +11,6 @@ function newDropdown() {
     <Dropdown.Menu>
         <Dropdown.Item href="#/action-1">Tim</Dropdown.Item>
         <Dropdown.Item href="#/action-2">Poslovna documentacija</Dropdown.Item>
-        <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
       </Dropdown.Menu>
     </Dropdown>
 );
