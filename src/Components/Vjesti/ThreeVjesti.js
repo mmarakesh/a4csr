a675985ebf6b341490d602980f81fe15805ae6c9
OxySokolova <poksanaa16@gmail.com>
2024-03-16 17:03:41 -0500

    all vjesti

--- src/Components/Vjesti/ThreeVjesti.js
+++ src/Components/Vjesti/ThreeVjesti.js
@@ -1,16 +1,16 @@
 import React from "react";
 import vjestiList from "./vjestiList";
-import { Link } from 'react-router-dom';
+
 
 function Vjesti() {
-  const firstThreeVjesti = vjestiList.slice(0, 3); // Get the first three items from vjestiList
+  const firstThreeVjesti = vjestiList.filter((_, id)=>[3,6,9].includes(id)); // Get the first three items from vjestiList
 
   return (
     <div>
       {firstThreeVjesti.map((element) => {
         const { id, img, title, description, city, date, link } = element;
         return (
-          <div>
+    
           <div key={id} className="container-vjesti">
             <img src={img} width="300px" alt="img news" />
             <h3>{title}</h3>
@@ -18,8 +18,8 @@ function Vjesti() {
             <button onClick={() => window.open(link)}>Pročitajte više</button>
             <p>{city}, {date}</p>
           </div>
-          <Link to = "/vjesti" className="link">All Vjesti</Link>
-          </div>
+          
+
         );
       })}
     </div>
