b0f441a9d3000f2c2d9139ffd8e1351a348b454e
mmarakesh <marinamakarova749@gmail.com>
2024-03-19 11:17:50 +0100

    fixed bug

--- src/Components/Vjesti/ThreeVjesti.js
+++ src/Components/Vjesti/ThreeVjesti.js
@@ -5,16 +5,20 @@ import "./Vjesti.css";
 
 function ThreeVjesti() {
   const firstThreeVjesti = vjestiList.filter((_, id)=>[2,4,7].includes(id)); // Get the first three items from vjestiList
-  const [showMore, setShowMore] = useState(false);
+  const [showText, setShowText] = useState(false);
+  const showTextClick = (element) => {
+    element.showMore = !element.showMore
+    setShowText(!showText)
+}
   return (
     <>
       <h1 className="vjesti-header">Naši <span className='green-header'> vjesti</span></h1>
     <div className="vjesti-three-all">
       {firstThreeVjesti.map((element) => {
-        const { id, img, title, description } = element;
+        const { id, img, title, description, showMore } = element;
         return (
     
-          <div key={id} className="vjesti-three-card" onClick={() => setShowMore(!showMore)}>
+          <div key={id} className="vjesti-three-card" onClick={() => showTextClick(element)}>
             <img src={img} width="440px" alt="img news" />
             <h3 className="vjesti-title">{title}</h3>
             <p className="vjesti-description">{showMore ? description : description.substring(0, 0)}</p>
