05089dda9a50254858f2964d104f631c52debee5
mmarakesh <marinamakarova749@gmail.com>
2024-03-20 20:40:20 +0100

    fixed bug

--- src/Components/Tim/Tim.js
+++ src/Components/Tim/Tim.js
@@ -18,37 +18,37 @@ function Tim() {
         <img src={image01} alt='jovana' class="bd-placeholder-img rounded-circle" width="200" height="200" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <h2 class="fw-normal">Jovana Bošković</h2>
         <p>Osnivačka članica</p>
-        <p><a class="btn btn-success" href="mailto: boskovicjovana1@gmail.com">Pošaljite mail »</a></p>
+        <p><a class="btn btn-success" href="mailto: boskovicjovana1@gmail.com" target='_blanck'>Pošaljite mail »</a></p>
       </div>
       <div class="col-lg-4 tim-card">
       <img src={image02} alt='jovana' class="bd-placeholder-img rounded-circle" width="200" height="200" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <h2 class="fw-normal">Nina Perunovic</h2>
         <p>Osnivačka članica</p>
-        <p><a class="btn btn-success" href="https://www.linkedin.com/in/nina-perunovic-0707/">Linkedin »</a></p>
+        <p><a class="btn btn-success" href="https://www.linkedin.com/in/nina-perunovic-0707/" target='_blanck'>LinkedIn »</a></p>
       </div>
       <div class="col-lg-4 tim-card">
       <img src={image03} alt='jovana' class="bd-placeholder-img rounded-circle" width="200" height="200" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <h2 class="fw-normal">Katya Letunovsky</h2>
         <p>Osnivačka članica</p>
-        <p><a class="btn btn-success" href="https://www.linkedin.com/in/katyaletunovsky/">Linkedin »</a></p>
+        <p><a class="btn btn-success" href="https://www.linkedin.com/in/katyaletunovsky/" target='_blanck'>LinkedIn »</a></p>
       </div>
       <div class="col-lg-4 tim-card">
       <img src={image04} alt='jovana' class="bd-placeholder-img rounded-circle" width="200" height="200" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <h2 class="fw-normal">Vanja (Abramovic) Jankovic</h2>
         <p>Osnivačka članica</p>
-        <p><a class="btn btn-success" href="https://www.linkedin.com/in/vanja-jankovic-8b826a118/">Linkedin »</a></p>
+        <p><a class="btn btn-success" href="https://www.linkedin.com/in/vanja-jankovic-8b826a118/" target='_blanck'>LinkedIn »</a></p>
       </div>
       <div class="col-lg-4 tim-card">
       <img src={image05} alt='jovana' class="bd-placeholder-img rounded-circle" width="200" height="200" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <h2 class="fw-normal">Antonina Berkivska</h2>
         <p>Volonterka, umrežavanje i savjetovanje</p>
-        <p><a class="btn btn-success" href="https://www.linkedin.com/in/antonina-berkivska/">Linkedin »</a></p>
+        <p><a class="btn btn-success" href="https://www.linkedin.com/in/antonina-berkivska/" target='_blanck'>LinkedIn »</a></p>
       </div>
       <div class="col-lg-4 tim-card">
       <img src={image06} alt='jovana' class="bd-placeholder-img rounded-circle" width="200" height="200" xmlns="http://www.w3.org/2000/svg" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"></img>
         <h2 class="fw-normal">Marina Makarova</h2>
         <p>Volonterka, razvoj web stranice</p>
-        <p><a class="btn btn-success" href="https://www.linkedin.com/in/marina-makarova-developer/">Linkedin »</a></p>
+        <p><a class="btn btn-success" href="https://www.linkedin.com/in/marina-makarova-developer/" target='_blanck'>LinkedIn »</a></p>
       </div>
     </div>
 
