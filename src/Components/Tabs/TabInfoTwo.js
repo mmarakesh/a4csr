ae5a674ccc8abb8770cfd3616b27eea44c78c136
mmarakesh <marinamakarova749@gmail.com>
2024-03-21 01:57:43 +0100

    fixed bug

--- src/Components/Tabs/TabInfoTwo.js
+++ src/Components/Tabs/TabInfoTwo.js
@@ -12,8 +12,8 @@ const TabInfoTwo = () => {
                     <>
                         <div key={id} className="medija-card">
                         <iframe width="700" height="350" src={video} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-                        <h3 className="medija-title">{title}</h3>
-                        <p>{date}</p>
+                        <h2 className="medija-title">{title}</h2>
+                        <p className="medija-date">{date}</p>
                         </div>
                         </>
                     
