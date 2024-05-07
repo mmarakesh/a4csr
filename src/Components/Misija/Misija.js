d0392cefeaaed9cc59115d000fb9780326121383
mmarakesh <marinamakarova749@gmail.com>
2024-05-07 11:06:54 +0200

    fixed bug

--- src/Components/Misija/Misija.js
+++ src/Components/Misija/Misija.js
@@ -4,7 +4,7 @@ import image from './cuate1.jpg';
 
 function Misija() {
     const[showMore, setShowMore] = useState(false);
-    let misijaText = `Naša misija je promovisanje principa društveno odgovornog i održivog poslovanja, kao i doprinos integraciji Crne Gore i zemalja Zapadnog Balkana. 
+    let misijaText = `Naša misija je promovisanje principa društveno odgovornog i održivog poslovanja, kao i doprinos integraciji Crne Gore. 
     Kroz usklađivanje ekonomskog rasta, socijalne inkluzije i zaštite životne sredine, te promociju ljudskih, građanskih, ekonomskih i socijalnih prava, posebno u privatnom sektoru, nastojimo stvoriti okruženje koje podstiče aktivno učešće mladih i zainteresovanih strana.
     Kroz saradnju sa relevantnim subjektima i razvoj civilnog društva na lokalnom i državnom nivou, želimo postići razmjenu iskustava, pružiti stručnu pomoć i pratiti stanje održivog razvoja u skladu sa UN globalnim ciljevima.
     Sve ovo radimo s ciljem unapređenja društveno odgovornog poslovanja i održivog razvoja, ističući važnost relevantnih pregovaračkih poglavlja i aktivnosti za postizanje održivog zajednice.`
